5622c20b772807c6ffb3b4248f200a0ed9295c63
bubumariia <mariiakudaiberdieva@gmail.com>
2025-07-21 20:57:57 +0300

    practicing DataTypes

---
+++ src/DataTypes.java
@@ -0,0 +1,12 @@
+public class DataTypes {
+    public static void main(String[] args){
+        boolean firstBoolean = false;
+        if(firstBoolean == true){
+            System.out.println("Boolean is True");
+        }
+        System.out.println("Boolean is False");
+        char firstChar = 'k';
+        System.out.println("First character is "+ firstChar);
+
+    }
+}
