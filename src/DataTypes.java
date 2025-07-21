9435b4d60d49b546c28133037c0f746d893a1f83
bubumariia <mariiakudaiberdieva@gmail.com>
2025-07-21 21:43:08 +0300

    added the other data types

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -5,8 +5,20 @@ public class DataTypes {
             System.out.println("Boolean is True");
         }
         System.out.println("Boolean is False");
+
         char firstChar = 'k';
-        System.out.println("First character is "+ firstChar);
+        System.out.println("First character is "+firstChar+ " " + (int)firstChar);
+
+        byte firstByte = 127;
+        System.out.println("First byte " + firstByte );
+
+        short firstShort = 17783;
+        System.out.println("First short  is " + firstShort);
+
+        int firstInt = 32000;
+        System.out.println(firstInt);
+
+
 
     }
 }
